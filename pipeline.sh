ac526d90ca18f2cb84669faa6be763f5abe88c6e
Nedeljko Ned Scepanovic <63357450+zeenmc@users.noreply.github.com>
2021-03-07 22:21:14 -0500

    Update pipeline.sh
    Update 03/07/2021-1

--- pipeline.sh
+++ pipeline.sh
@@ -5,19 +5,21 @@
 
 # docker pull myproject/qaauto:latest
 
-
 # Missing port options, without port(-p) application will not be accessible, 
-# We don't know on which port (80,443 or some other port) application is expecting traffic
-#Also I give name for running container as is easier to work with names then with hash values
+# We don't know on which port (80,443 or some other port) application is expecting traffic.
+#Also I gave name for running container as is easier to work with names then with hash values
 
 docker run -d  --name qaauto -v qalogs:/qaauto/logs myproject/qaauto:latest
 
 # I assumed host has public ip address
 # with cURL command we will fetch host's public ip address, 
-# in case of private ip address, we will use regularex to find output
-# from ifconfig or ip add show
 
-QUAATO_IP = curl ifconfig.me
+QUAATO_IP=curl ifconfig.me
+
+#This command will give output of ip address from eth0 NIC, 
+#also we can use if our host use private ip address
+
+#QUAATO_IP=$(ip -f inet -o addr show eth0 | cut -d\  -f 7 | cut -d/ -f 1)
 
 
 # With docker exec we will run test automatisation inside of running container 
@@ -35,15 +37,19 @@ FILENAME=/var/lib/docker/volumes/qalogs/_data/qhauto-$(date +%Y-%m-%d).log
 ##############Vars#############
 ##########Functions############
 
+#In this function we can implement return codes for exit, in that case we will know why script exit
+
 function stopremove {
 docker stop qaauto
 docker rmi qaauto:latest
+echo "Docker container is stopped, and image have been removed"
 exit
 }
 
 ##########Functions############
 
-if [ -f /var/lib/docker/volumes/qalogs/_data/qhauto-$(date +%Y-%m-%d).log ]
+#this if statement is verifying existence of a log file, and is it from today
+if [ -f "$FILENAME" ]
 
 then
         echo "Log file have been created, TEST PASSED"
@@ -53,6 +59,10 @@ else
         stopremove
 fi
 
+#Checking how many bytes log file has
+#0 (zero) means, file is empty
+#everything else, mean file is NOT empty
+# FYI word FAIL has 5 bytes
 
 FILESIZE=$(stat -c%s "$FILENAME")
 
@@ -61,16 +71,16 @@ echo "Size of log file: $FILENAME is $FILESIZE byte"
 if [ "$FILESIZE" -eq "0" ]
 
 then
-     echo "file is empty, TEST FAILED!!!, "
+     echo "Log file is empty, TEST FAILED!!!, "
      stopremove
 
 else
-     echo "file is NOT empty"
+     echo "Log file is NOT empty"
 fi
 
 
 #Checking if string FAIL exists in log file
-if grep -q "FAIL" $FILENAME
+if grep -q "FAIL" "$FILENAME"
 
 then
       echo "Failure, TEST FAILED!!!"
@@ -79,5 +89,5 @@ else
       echo "Success"
 fi
 
-
+#END OF THE SCRIPT
 stopremove
